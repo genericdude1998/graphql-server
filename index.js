1629fb155b3cc9c4f6ef6d217ded7db29fabef1a
example <you@example.com>
2024-02-01 13:48:10 +0000

    full CRUD

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const courses = [
+  { name: "Graph", price: 10 },
+  { name: "React", price: 15 },
+];
+
 const typeDefs = gql`
   type Query {
     welcome: String
@@ -12,6 +17,22 @@ const typeDefs = gql`
     name: String!
     price: Float!
   }
+
+  input AddCourseInput {
+    name: String!
+    price: Float!
+  }
+
+  input UpdateCourseInput {
+    name: String!
+    price: Float!
+  }
+
+  type Mutation {
+    addCourse(input: AddCourseInput): Course
+    deleteCourse(name: String): [Course]
+    updateCourse(input: UpdateCourseInput): Course
+  }
 `;
 
 const resolvers = {
@@ -19,23 +40,41 @@ const resolvers = {
     welcome: (_, __, context) => {
       return `Welcome to ${context.country}`;
     },
-    course: (parent, args, context) => {
-      return [
-        { name: "Graph", price: 10 },
-        { name: "React", price: 15 },
-      ].find((c) => c.name === args.name);
+    course: (parent, args, { courses }) => {
+      return courses.find((c) => c.name === args.name);
     },
-    courses: () => {
-      return [
-        { name: "Graph", price: 10 },
-        { name: "React", price: 15 },
-      ];
+    courses: (_, __, { courses }) => {
+      return courses;
+    },
+  },
+  Mutation: {
+    addCourse: (parent, { input: { name, price } }, { courses }) => {
+      console.log("Creating", name);
+      console.log({ name, price });
+      courses.push({ name, price });
+      return { name, price };
+    },
+
+    updateCourse: (parent, { input: { name, price } }, { courses }) => {
+      console.log("Updating", { name, price });
+      const courseToUpdate = courses.find((c) => c.name === name);
+      courseToUpdate.price = price;
+      return courseToUpdate;
+    },
+    deleteCourse: (parent, { name }, { courses }) => {
+      console.log("Deleting", name);
+      courses = courses.filter((course) => course.name !== name);
+      courses.pop();
+      return courses;
     },
   },
 };
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: { country: "US" },
+  context: {
+    country: "US",
+    courses,
+  },
 });
 server.listen().then(({ url }) => console.log(`Server is running at ${url}`));
